ae135e21ba1d540f5f0d5ca1ff91c74615cb3273
prabeshkunwar12 <prabeshkunwar12@gmail.com>
2025-05-24 23:22:34 -0400

    sidebar implemented

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+import SideBar from "@/components/SideBar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedInUser = {firstName: 'Prabesh', lastName: 'Kunwar'}
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <SideBar user={loggedInUser} />
         {children}
     </main>
   );
