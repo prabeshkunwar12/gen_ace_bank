e5c64e467238411aff80861a21b996bb912bf0da
prabeshkunwar12 <prabeshkunwar12@gmail.com>
2025-05-25 09:37:33 -0400

    Mobile sidebar implemented

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import SideBar from "@/components/SideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -11,7 +13,20 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
         <SideBar user={loggedInUser} />
-        {children}
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image 
+              src="/icons/logo.svg"
+              alt="logo"
+              width={30}
+              height={30}
+            />
+            <div>
+              <MobileNav user={loggedInUser} />
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
